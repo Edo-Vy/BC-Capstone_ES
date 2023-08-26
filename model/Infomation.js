389b4b9f4d09e93fd76dd022cfba0fc97e3d260d
Edo-Vy <tranthaovy8898@gmai.com>
2023-08-27 01:21:37 +0700

    register

---
+++ model/Infomation.js
@@ -0,0 +1,8 @@
+function Infomation() {
+        this.email = "",
+        this.password = "",
+        this.passwordConfirm = '',
+        this.name = "",
+        this.gender = "",
+        this.phone = ""
+}
